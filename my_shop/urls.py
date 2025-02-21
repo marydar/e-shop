12f97e7393ef1b86d8554047061d9ff109c42921
Maryam Dar <maryamdar1384@gmail.com>
2025-02-21 19:25:51 +0330

    token , jwt

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -19,10 +19,16 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("shop_app.urls")),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
